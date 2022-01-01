7adc03ab2a2e1a52dab7284cffce936ab5be28a2
wanderdeivid <wander.deivid@hotmail.com>
2022-01-01 09:18:59 -0300

    Adiciona os exemplos de introdução ao DOM e corrige o problema da Aula 5 relacionado ao contador.

--- Aula-5/assets/js/scripts.js
+++ Aula-5/assets/js/scripts.js
@@ -1,14 +1,14 @@
 /* Contador
 /* Declara uma variável, faz ele ir no documento html e pegar um elemento pelo seu ID(Cada elemento tem um ID único) */
-var currentNumberWrapper = document.getElementById('currentNumber');
-var currentNumber = 0;
+var currentNumberWrapper = document.getElementById('atual');
+var atual = 0;
 
 function increment(){
-    currentNumber = currentNumber + 1;
-    currentNumberWrapper.innerHTML = currentNumber;
+    atual += 1;
+    currentNumberWrapper.innerHTML = atual;
 }
 
 function decrement(){
-    currentNumber = currentNumber - 1;
-    currentNumberWrapper.innerHTML = currentNumber;
+    atual -= 1;
+    currentNumberWrapper.innerHTML = atual;
 }
